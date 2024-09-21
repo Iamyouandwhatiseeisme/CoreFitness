9116689168147918c849cd5ec23d6a5320cfe159
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-09-21 19:05:46 +0400

    add header navigation elements

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            dasad
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+const Header = () => {
+    return ( 
+        <div className="header">
+        <nav>
+            <ul className="navigation-links">
+                <li>Photo Content</li>
+                <li>Audio Content</li>
+                <li>Information</li>
+            </ul>
+
+        </nav>
+            
+        </div>
+     );
+}
+ 
+export default Header;
