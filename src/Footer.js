ad0e3af419096062ddd38ec620c7066b1d0b8a21
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-09-22 21:44:32 +0400

    add footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,15 @@
+import "./Footer.css";
+import React from "react";
 const Footer = () => {
     return ( 
         <div className="footer">
-            dasad
+        <nav>
+            <ul className="footer-list">
+                <li>About</li>
+                <li>Contact</li>
+                <li>All rights reserved {new Date().getFullYear()}</li>
+            </ul>
+        </nav>   
         </div>
      );
 }
