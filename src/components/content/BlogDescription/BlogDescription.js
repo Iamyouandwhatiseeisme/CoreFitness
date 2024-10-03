3cc4f70567cf7e3ac10517adff9f4dbec86c415e
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-03 20:10:19 +0400

    comp(blogpost): add blogpost component

---
+++ src/components/content/BlogDescription/BlogDescription.js
@@ -0,0 +1,11 @@
+export default function BlogDesription ({heading, description}) {
+    const descriptionParagraphs = [...description];
+    return(
+        <div>
+            <h1>{heading}</h1>
+            {descriptionParagraphs.map((paragraph, index)=>{
+                return <p key={index}>{paragraph}</p>
+            })}
+        </div>
+    )
+}
