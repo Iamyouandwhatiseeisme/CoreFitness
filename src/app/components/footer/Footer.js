c07e76475c8c5ad93783cc62a8b8085bd8a1d17e
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-08 21:36:10 +0400

    refractor(navigation): move post,product,blog nav links to header from footer

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -9,8 +9,7 @@ const Footer = () => {
                     <Link href="/"><li>Home</li></Link>
                     <Link href="/about"><li>About</li></Link>
                     <Link href="/contact"><li>Contact</li></Link>
-                    <Link href="/blog"><li>Blog</li></Link>
-                    <Link href="/products"><li>Products</li></Link>
+                    
                     <li>All rights reserved {new Date().getFullYear()}</li>
                 </ul>
             </nav>   
--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -10,6 +10,9 @@ const Header = () => {
                     <Link href="/profile"><li>Profile</li></Link>
                     <Link href="/settings"><li>Settings</li></Link>
                     <Link href="/cart"><li>Cart</li></Link>
+                    <Link href="/blog"><li>Blog</li></Link>
+                    <Link href="/products"><li>Products</li></Link>
+                    <Link href="/posts"><li>Posts</li></Link>
                 </ul>
 
             </nav>
