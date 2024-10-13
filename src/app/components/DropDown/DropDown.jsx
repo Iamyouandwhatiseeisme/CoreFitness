d7757e0f978a9a58383e4232b6aacad3f1f894d0
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-13 16:09:42 +0400

    refr(discard): discard dropdownmenu being discarded on click outside function

--- src/app/components/DropDown/DropDown.jsx
+++ src/app/components/DropDown/DropDown.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState} from "react"
 import DropDownButton from "../DropDownButton/DropDownButton";
 import DropDownContent from "../DropDownContent/DropDownContent";
 import "./DropDown.css"
@@ -9,25 +9,11 @@ const DropDown = ({buttonText, content}) => {
     const toggleDropDown = () =>{
         setOpen((open)=> !open);
     }
-    const dropDownRef = useRef();
 
-    useEffect(()=>{
-        const handler = (event) => {
-            if(dropDownRef.current && !dropDownRef.current.contains(event.target)){
-                setOpen(false);
-
-            }
-            document.addEventListener("click", handler)
-
-            return () =>{
-                document.removeEventListener("click", handler);
-            }
-        }
-
-    }, [dropDownRef])
+   
 
     return (
-        <div className="dropdown" ref={dropDownRef}>
+        <div className="dropdown">
             <DropDownButton toggle={toggleDropDown} open={open}>
             {buttonText}
 
