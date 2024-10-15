f793472701bd2f1c87744ee56ee3002c01506f4c
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-15 22:01:47 +0400

    refr(fetch): fetching moved to a separate function, cleaned up server components

---
+++ src/app/fetcher/fetchProducts.jsx
@@ -0,0 +1,22 @@
+import NotFound from "../notfound/NotFound";
+
+export default async function fetchProducts ({fetchItemType, debouncedSearch, sortOption, sortOrder}){
+    try {
+        let url = `https://dummyjson.com/${fetchItemType}`;
+        if(debouncedSearch){
+            url = `https://dummyjson.com/${fetchItemType}/search?q=${debouncedSearch}`;
+        }
+        if(sortOption && sortOrder){
+            url = `https://dummyjson.com/${fetchItemType}?sortBy=${sortOption}&order=${sortOrder}`
+        }
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.json();
+        return fetchItemType === "posts" ? (data.posts || []) : (data.products || [])
+    } catch (error) {
+        console.log("Error fetching posts: ", error)
+        
+        return <NotFound page="posts"></NotFound>
+        
+    }
+}
