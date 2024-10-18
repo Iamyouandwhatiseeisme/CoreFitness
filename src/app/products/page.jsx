cca9abd755bd7811111eac1a8f9d8399ad3267a1
NuskheladzeShota <nuskheladzeshota@gmail.com>
2024-10-19 02:04:42 +0400

    upload create prodact functional

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -6,10 +6,11 @@ import Link from "next/link";
 import DropDown from "../components/DropDown/DropDown";
 import SearchBar from "../components/SearchBar/SearchBar";
 import fetchProducts from "../fetcher/fetchProducts";
+import AddProductButton from "../components/AddProductButton/AddProductButton";
 
 
 
-export default async function Products ({searchParams}) {
+export default async function Products({ searchParams }) {
     const debouncedSearch = searchParams.search || "";
     const sortOption = searchParams.option || ""
     const sortOrder = searchParams.order || ""
@@ -17,56 +18,55 @@ export default async function Products ({searchParams}) {
 
 
     const sortOptions = [
-        {   label: 'Price: Low to High',
+        {
+            label: 'Price: Low to High',
             value: 'price-low-to-high',
             option: "price",
             order: "asc"
-            },
-        {   label: 'Price: High to Low', 
+        },
+        {
+            label: 'Price: High to Low',
             value: 'price-high-to-low',
             option: "price",
             order: "desc"
-            
-             },
-        {   label: 'Name: A-Z',  
+
+        },
+        {
+            label: 'Name: A-Z',
             value: 'name-ascending',
             option: "title",
             order: "asc"
-            },
-        {   label: 'Name: Z-A',  
+        },
+        {
+            label: 'Name: Z-A',
             value: 'name-descending',
             option: "title",
             order: "desc"
-            },
-      ];
-    
-    
-    var products = await  fetchProducts({fetchItemType, debouncedSearch, sortOption, sortOrder})
-    
-    
+        },
+    ];
 
-      if (products.length === 0) {
+    var products = await fetchProducts({ fetchItemType, debouncedSearch, sortOption, sortOrder })
+
+    if (products.length === 0) {
         return (
             <div className="loading-screen">
-                    <div className="app-bar">
-                     <Header />
+                <div className="app-bar">
+                    <Header />
                     <SearchBar searchItemType="Search Products" />
                 </div>
-                </div> 
-    ) 
-      }
+            </div>
+        )
+    }
     return (
-
-        
         <div className="products-page">
             <div className="app-bar">
                 <Header />
                 <SearchBar searchItemType="Search Posts" />
+                <AddProductButton />
             </div>
-            
-            <div className="dropdown-menu"><DropDown buttonText="Sort Products By:"content={sortOptions}></DropDown></div>
+            <div className="dropdown-menu"><DropDown buttonText="Sort Products By:" content={sortOptions}></DropDown></div>
             <div className="products-list">
-                {products.map((product)=>{
+                {products.map((product) => {
                     return (
                         <Link key={product.id} href={`/products/${product.id}`}>
                             <div className="product-card">
@@ -76,8 +76,6 @@ export default async function Products ({searchParams}) {
                                 <div className="product-info">Price: {product.price}$</div>
                             </div>
                         </Link>
-
-
                     )
                 })}
             </div>
