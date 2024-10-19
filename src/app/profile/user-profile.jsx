8a5dc721447bc7da2a2b5ce349fb04995721944f
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-20 02:16:05 +0400

    feat(add): add main user content

--- src/app/profile/user-profile.jsx
+++ src/app/profile/user-profile.jsx
@@ -27,15 +27,50 @@ export async function UserProfile() {
             height: "50%",
             background: "black",
             display: "flex",
+            // flexDirection: "column"
+            justifyContent: "space-between"
+          }}>
+            <div style={{
+            width: "30%",
+            height: "100%",
+            background: "aqua",
+            display: "flex",
             flexDirection: "column"
-          }}></div>
+          }}>
+              <div>{user.company.title}</div> 
+              <div>Title</div> 
+            </div>
+            <div style={{
+            width: "30%",
+            height: "100%",
+            background: "blue",
+            display: "flex",
+            flexDirection: "column"
+          }}>
+            <div>{user.company.name}</div> 
+            <div>Company name</div>
+          </div>
+            <div style={{
+            width: "30%",
+            height: "100%",
+            background: "yellow",
+            display: "flex",
+            flexDirection: "column"
+          }}>
+            <div>{user.company.department}</div> 
+            <div>Department</div>
+          </div>
+          </div>
             <div style={{
             width: "100%",
             height: "50%",
             background: "red",
             display: "flex",
             flexDirection: "column"
-          }}></div>
+          }}>
+            <div>Phone: {user.phone}</div> 
+            <div>Email: {user.email}</div> 
+          </div>
 
           </div>
 
