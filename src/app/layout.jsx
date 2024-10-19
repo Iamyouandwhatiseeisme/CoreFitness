e3d4475b9155aa2865a1f93c4f0b2e73bdef61b3
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-19 13:58:49 +0400

    refr(cleanup): clean up layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,17 +4,13 @@ export const metadata = {
     description: 'Web site created with Next.js.',
   }
 export default function RootLayout({ children }) {
+  
     return (
         <html lang="en">
   <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="Web site created using create-react-app"
-    />
+    
   
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
     
     <title>React App</title>
   </head>
