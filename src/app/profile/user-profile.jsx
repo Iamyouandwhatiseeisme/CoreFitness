6e31adf2127661cddb44f42d5d645686a6561403
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-20 00:42:43 +0400

    refr(): correct user.firstname property

--- src/app/profile/user-profile.jsx
+++ src/app/profile/user-profile.jsx
@@ -4,7 +4,7 @@ export async function UserProfile() {
   const User = await fetchUser();
   return (
     <div className="profile-card">
-        <h1>Name: {User.name}</h1>
+        <h1>Name: {User.firstName}</h1>
         <h1>Last Name: {User.lastName}</h1>
         <p>Email: {User.email}</p>
         <img src={User.image} style={{width:"250px"}} alt="User-image"></img>
