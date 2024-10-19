2d2422f64401d046f3489f0ae8b6158fe9f71f0f
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-20 00:39:00 +0400

    refr(): replace divs with UserProfile component in page.gs

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,7 @@
 import User from "../components/data/User"
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import { UserProfile } from "./user-profile";
 
 import "./index.css"
 
@@ -9,13 +10,7 @@ export default function Profile (){
     return (
         <>
         <Header />
-            <div className="profile-card">
-                <h1>Name: {User.name}</h1>
-                <h1>Last Name: {User.lastName}</h1>
-                <p>Email: {User.email}</p>
-                <img src={User.Image.src} style={{width:"250px"}} alt="User-image"></img>
-                
-            </div>
+        <UserProfile />
         <Footer />
         </>
     )
--- src/app/profile/user-profile.jsx
+++ src/app/profile/user-profile.jsx
@@ -1,5 +1,14 @@
-export function UserProfile() {
+import {fetchUser} from "../fetcher/fetchUser"
+
+export async function UserProfile() {
+  const User = await fetchUser();
   return (
-    <div>user-profile</div>
+    <div className="profile-card">
+        <h1>Name: {User.name}</h1>
+        <h1>Last Name: {User.lastName}</h1>
+        <p>Email: {User.email}</p>
+        <img src={User.Image.src} style={{width:"250px"}} alt="User-image"></img>
+        
+    </div>
   )
 }
