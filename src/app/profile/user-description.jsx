0638d49fb886db395c85a4781709ffc17daff2d5
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-20 05:51:43 +0400

    refr(component): update user-description with BorderDiv components

--- src/app/profile/user-description.jsx
+++ src/app/profile/user-description.jsx
@@ -1,4 +1,5 @@
 import { UserDescriptionComponent } from "./description-component"
+import { BorderDiv } from "./border-div"
 
 export function UserDescription(props) {
   const user = props.user;
@@ -6,48 +7,10 @@ export function UserDescription(props) {
   return (
     <div className="userDescription">
       <UserDescriptionComponent descProp="Title" descValue={user.company.title}/>
-      {/* <div style={{
-      width: "30%",
-      height: "100%",
-      // background: "aqua",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "end",
-      alignItems: "center"
-    }}>
-        <b style={{fontSize: "24px", textAlign: "center"}}>{user.company.title}</b> 
-        <div style={{paddingTop: "10px"}}>Title</div> 
-      </div> */}
-      
-      <div style={{ background: "gray", width: "2px"}}></div>
-      
-      <div style={{
-        width: "30%",
-        height: "100%",
-        // background: "blue",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "end",
-        alignItems: "center"
-      }}>
-      <b style={{fontSize: "24px", textAlign: "center"}}>{user.company.name}</b> 
-      <div style={{paddingTop: "10px"}}>Company name</div>
-      </div>
-
-      <div style={{ background: "gray", width: "2px"}}></div>
-      
-      <div style={{
-          width: "30%",
-          height: "100%",
-          // background: "yellow",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "end",
-          alignItems: "center"
-        }}>
-        <b style={{fontSize: "20px", textAlign: "center"}}>{user.company.department}</b> 
-        <div style={{paddingTop: "10px"}}>Department</div>
-      </div>
+      <BorderDiv />
+      <UserDescriptionComponent descProp="Company name" descValue={user.company.name}/>
+      <BorderDiv />
+      <UserDescriptionComponent descProp="Department" descValue={user.company.department}/>
     </div>
   )
 }
