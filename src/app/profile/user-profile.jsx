82457425ba6be2ded73e24e03023f8f28babcf21
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-21 23:12:43 +0400

    refr(content): remove unused content

--- src/app/profile/user-profile.jsx
+++ src/app/profile/user-profile.jsx
@@ -1,18 +1,12 @@
-// import {fetchUser} from "../fetcher/fetchUser"
 import { AvatarName} from "./user-avatar-name"
 import { UserBackground } from "./user-background"
 
 
 export function UserProfile({user}) {
-  // const user = await fetchUser(currentUser.id);
-  // console.log("user", user);
   return (
     <div className="profile-card">
-        
         <AvatarName user={user}/>
         <UserBackground user={user}/>
-        
-        
     </div>
   )
 }
