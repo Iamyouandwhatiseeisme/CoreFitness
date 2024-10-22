bef333bd312c7046be9be4eed7a4bcf8c94501ba
NuskheladzeShota <nuskheladzeshota@gmail.com>
2024-10-23 01:59:55 +0400

    upload AddButton

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer"
 import Link from "next/link";
@@ -5,19 +6,18 @@ import "./index.css"
 import SearchBar from "../components/SearchBar/SearchBar";
 import DropDown from "../components/DropDown/DropDown";
 import fetchProducts from "../fetcher/fetchProducts";
-
-
+import AddButton from '../components/AddButton/AddButton';
 
 async function Posts ({searchParams}) {
-   
     const debouncedSearch = searchParams.search || "";
     const sortOption = searchParams.option || ""
     const sortOrder = searchParams.order || ""
     const fetchItemType = "posts"
 
-
+    const handlePostClick = () => {
+        alert('Post action triggered!');
+      };
     
-
     const sortOptions = [
         {   label: 'Views: Low to High',
             value: 'vews-low-to-high',
@@ -43,21 +43,14 @@ async function Posts ({searchParams}) {
     
       var posts = await fetchProducts({fetchItemType, debouncedSearch, sortOption, sortOrder})
     
-
-    
       if (posts.length === 0) {
         return (
             <div className="posts-page">
-
-                    
                     <div className="app-bar">
                     <Header />
                     <SearchBar searchItemType="Search Posts" />
-
-                    
             </div>
             <h2>Search query empty...</h2>
-
                 <Footer />
             </div> 
     ) 
@@ -67,8 +60,8 @@ async function Posts ({searchParams}) {
         <div className="app-bar">
             <Header />
             <SearchBar searchItemType="Search Posts" />
+            <AddButton item="Posts" onClick={handlePostClick} />
         </div>
-            
             <div className="dropdown-menu"><DropDown buttonText="Sort Products By:"content={sortOptions}></DropDown></div>
             <div className="posts-list">
                 {posts.map((post)=>{
@@ -77,31 +70,22 @@ async function Posts ({searchParams}) {
                         <div className="posts-card">
                             <div className="posts-info"><strong>{post.title}</strong></div>
                             <div className="posts-info">{post.body}</div>
-                           
                             <div>
                                 <span className="posts-info">Views: {post.views}</span>
                                 <span className="posts-info">Likes: {post.reactions.likes}</span>
                                 <span className="posts-info">Dislikes: {post.reactions.dislikes}</span>
-
                             </div>
                             <div className="posts-info">Post Id: {post.id}</div>
                             <div className="posts-info">Tags: {post.tags.map((tag)=>{
                                 return (
                                     <span className="post-tag">#{tag}</span>
                                 )
-                                
                             })}</div>
-
-
                         </div>
                         </Link>
-
-
-
                     )
                 })}
             </div>
-
             <Footer />
         </div>
     )
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,4 +1,4 @@
-
+'use client'
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer"
 import "./index.css"
@@ -6,67 +6,65 @@ import Link from "next/link";
 import DropDown from "../components/DropDown/DropDown";
 import SearchBar from "../components/SearchBar/SearchBar";
 import fetchProducts from "../fetcher/fetchProducts";
+import AddButton from '../components/AddButton/AddButton'
 
-
-
-export default async function Products ({searchParams}) {
+export default async function Products({ searchParams }) {
     const debouncedSearch = searchParams.search || "";
     const sortOption = searchParams.option || ""
     const sortOrder = searchParams.order || ""
     const fetchItemType = "products"
 
-
+    const handleProductClick = () => {
+        alert('Product action triggered!');
+    };
     const sortOptions = [
-        {   label: 'Price: Low to High',
+        {
+            label: 'Price: Low to High',
             value: 'price-low-to-high',
             option: "price",
             order: "asc"
-            },
-        {   label: 'Price: High to Low', 
+        },
+        {
+            label: 'Price: High to Low',
             value: 'price-high-to-low',
             option: "price",
             order: "desc"
-            
-             },
-        {   label: 'Name: A-Z',  
+
+        },
+        {
+            label: 'Name: A-Z',
             value: 'name-ascending',
             option: "title",
             order: "asc"
-            },
-        {   label: 'Name: Z-A',  
+        },
+        {
+            label: 'Name: Z-A',
             value: 'name-descending',
             option: "title",
             order: "desc"
-            },
-      ];
-    
-    
-    var products = await  fetchProducts({fetchItemType, debouncedSearch, sortOption, sortOrder})
-    
-    
-
-      if (products.length === 0) {
+        },
+    ];
+    var products = await fetchProducts({ fetchItemType, debouncedSearch, sortOption, sortOrder })
+    if (products.length === 0) {
         return (
             <div className="loading-screen">
-                    <div className="app-bar">
-                     <Header />
+                <div className="app-bar">
+                    <Header />
                     <SearchBar searchItemType="Search Products" />
                 </div>
-                </div> 
-    ) 
-      }
+            </div>
+        )
+    }
     return (
-
-        
         <div className="products-page">
             <div className="app-bar">
                 <Header />
                 <SearchBar searchItemType="Search Posts" />
+                <AddButton item="Products" onClick={handleProductClick} />
             </div>
-            
-            <div className="dropdown-menu"><DropDown buttonText="Sort Products By:"content={sortOptions}></DropDown></div>
+            <div className="dropdown-menu"><DropDown buttonText="Sort Products By:" content={sortOptions}></DropDown></div>
             <div className="products-list">
-                {products.map((product)=>{
+                {products.map((product) => {
                     return (
                         <Link key={product.id} href={`/products/${product.id}`}>
                             <div className="product-card">
@@ -76,8 +74,6 @@ export default async function Products ({searchParams}) {
                                 <div className="product-info">Price: {product.price}$</div>
                             </div>
                         </Link>
-
-
                     )
                 })}
             </div>
