94f952bd9c96459264ba0ef7e397fefa8d862f39
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-25 00:24:18 +0400

    refr(del): delete alert box on logout

--- src/app/components/logoutButton/LoggoutButton.jsx
+++ src/app/components/logoutButton/LoggoutButton.jsx
@@ -1,23 +1,24 @@
-'use client';
+"use client";
 import { useRouter } from "next/navigation";
-import { logout } from '../../util/logout'
-import './logout.css'
-import React from 'react'
+import { logout } from "../../util/logout";
+import "./logout.css";
+import React from "react";
 
 export default function LoggoutButton() {
-    const router = useRouter();
-    
-    const handleLogout = async () => {
-        const result = await logout();
+  const router = useRouter();
 
-        if (result.success) {
-            alert(result.message);
-            router.push('/login');
-        } else {
-            alert(result.message || 'Logout failed.');
-        }
-    };
+  const handleLogout = async () => {
+    const result = await logout();
+
+    if (result.success) {
+      router.push("/login");
+    } else {
+      alert(result.message || "Logout failed.");
+    }
+  };
   return (
-      <button className="logout-btn" onClick={handleLogout}>Log Out</button>
-  )
+    <button className="logout-btn" onClick={handleLogout}>
+      Log Out
+    </button>
+  );
 }
