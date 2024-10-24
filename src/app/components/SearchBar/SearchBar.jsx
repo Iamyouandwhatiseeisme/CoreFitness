0d0a6d4f09b618901b6358330faf6f46f469f1fd
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-25 00:24:52 +0400

    refr(fix): fix searchbar not being triggered if searchinput is empty after searching

--- src/app/components/SearchBar/SearchBar.jsx
+++ src/app/components/SearchBar/SearchBar.jsx
@@ -1,30 +1,33 @@
-"use client"
-import { useState, useEffect } from "react"
-import { useRouter } from "next/navigation"
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import useDebounce from "../../hooks/useDebounce";
 
+export default function SearchBar({ searchItemType }) {
+  const [searchValue, setSearchValue] = useState("");
+  const debouncedValue = useDebounce(searchValue, 500);
+  const router = useRouter();
+  useEffect(() => {
+    if (debouncedValue) {
+      router.push(`?search=${debouncedValue}`);
+    }
+  }, [debouncedValue, router]);
 
-export default function SearchBar ({searchItemType}) {
-    const [searchValue, setSearchValue] = useState("");
-    const debouncedValue = useDebounce(searchValue, 500);
-    const router = useRouter();
-    useEffect(() => {
-        if (debouncedValue) {
-            router.push(`?search=${debouncedValue}`);
-        }
-    }, [debouncedValue, router]);
-
-    return (
-        <div>Search: <input 
-            type="text"
-            value={searchValue}
-            onChange={(e)=>{
-                setSearchValue(e.target.value);
-            }}
-            placeholder={searchItemType}
-            title="search"></input>
-            
-        </div>
-
-    )
+  return (
+    <div>
+      Search:{" "}
+      <input
+        type="text"
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+          if (e.target.value === "") {
+            router.push("/posts");
+          }
+        }}
+        placeholder={searchItemType}
+        title="search"
+      ></input>
+    </div>
+  );
 }
