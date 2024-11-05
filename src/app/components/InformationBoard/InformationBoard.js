c7fff572879eee83124efd36a62b02c1d0fc67dc
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-11-05 21:40:29 +0400

    refr(fix): fix hovered colors not staying if you take mouse off the item without hovering around other items

--- src/app/components/InformationBoard/InformationBoard.js
+++ src/app/components/InformationBoard/InformationBoard.js
@@ -4,7 +4,8 @@ import { informationBoardItems } from "../data/InformationBoardItems";
 import { hover } from "@testing-library/user-event/dist/hover";
 
 export default function InformationBoard() {
-  const [hovered, setHovered] = useState("0");
+  const [hovered, setHovered] = useState(0);
+  console.log(hovered);
   const hoverColors = [
     "#FBBF24",
     "#60A5FA ",
@@ -17,15 +18,22 @@ export default function InformationBoard() {
     <div className="flex flex-row h-90vh gap-20 justify-center mt-5 mb-5">
       <div className="hidden  flex-col rounded-2xl w-1/3 border border-header-hover-dark border-solid bg-black bg-opacity-85  xl:flex ">
         {informationBoardItems.map((item) => {
+          const isHovered = hovered === item.key;
           return (
             <div
               key={item.key}
               onMouseEnter={() => setHovered(item.key)}
               style={{
-                backgroundColor:
-                  hovered === item.key ? hoverColors[item.key] : "transparent",
+                backgroundColor: isHovered
+                  ? hoverColors[item.key]
+                  : "transparent",
+                transition: "transform 0.15s ease-in-out",
+                width: isHovered ? "550px" : "",
+                transform: isHovered ? "scale(1.05)" : "scale(1.0)",
+                height: isHovered ? "384px" : "",
+                transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1);",
               }}
-              className={` hover:w-info-board-hover-width rounded-3xl  hover:z-10 hover:h-96  transition-transform transform hover:scale-105 cursor-pointer`}
+              className={` rounded-3xl  hover:z-10 cursor-pointer`}
             >
               <div className="flex justify-start gap-10 m-8 flex-row h-14  ">
                 <div className="w-16 flex flex-col justify-center">
