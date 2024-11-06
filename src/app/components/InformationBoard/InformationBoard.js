81d67d005a63a20375948259076f97f9ac06c004
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-11-06 19:37:21 +0400

    refr(del): delete residue code in InformationBoard.js

--- src/app/components/InformationBoard/InformationBoard.js
+++ src/app/components/InformationBoard/InformationBoard.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { informationBoardItems } from "../data/InformationBoardItems";
-import { hover } from "@testing-library/user-event/dist/hover";
 
 export default function InformationBoard() {
   const [hovered, setHovered] = useState(0);
