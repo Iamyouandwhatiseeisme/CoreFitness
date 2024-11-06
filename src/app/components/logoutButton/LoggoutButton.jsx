165f148fe91bc8de8bbeedf5ca2c279d5a6db202
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-11-06 22:44:33 +0400

    refr(fix): fix loader not displaying on AuthenticationButton

--- src/app/components/logoutButton/LoggoutButton.jsx
+++ src/app/components/logoutButton/LoggoutButton.jsx
@@ -1,8 +1,13 @@
 import "./logout.css";
 import React from "react";
+import { ClipLoader } from "react-spinners";
 
 export default function AuthenticationButton({ href, type, buttonText }) {
-  return (
+  return buttonText === "Loading" ? (
+    <div className="h-10 w-20 items-center justify-center flex flex-col mt-2 text-black dark:text-white bg-gray-400 hover:bg-gray-500 dark:bg-header-hover-dark rounded-md cursor-pointer ">
+      <ClipLoader color="white"></ClipLoader>
+    </div>
+  ) : (
     <a href={href}>
       <button className="h-10 w-20 items-center mt-2 text-black dark:text-white bg-gray-400 hover:bg-gray-500 dark:bg-header-hover-dark rounded-md cursor-pointer ">
         {buttonText}
