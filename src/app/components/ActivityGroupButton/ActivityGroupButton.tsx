1d40698743e60c06146082d018c7a858bd25f540
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-11-16 14:13:13 +0400

    migrate(ts): chatwindow to ts

--- src/app/components/ActivityGroupButton/ActivityGroupButton.tsx
+++ src/app/components/ActivityGroupButton/ActivityGroupButton.tsx
@@ -1,25 +1,26 @@
 import { FormControl, MenuItem, Select } from "@mui/material";
 import React from "react";
+import { UserInfo } from "../types";
 
-export default function ActivityGroupButton({
-  activityData,
-  userInfo,
-  handleChange,
+export default function ActivityGroupButton(props: {
+  activityData: string[];
+  userInfo: UserInfo;
+  handleChange: (key: string, value: string) => void;
 }) {
   return (
     <div>
       <h1 className="font-serif">How many days a week do you exercise?</h1>
       <div className="w-1/2">
-        <FormControl color="neutral" className="bg-white " fullWidth>
+        <FormControl color="primary" className="bg-white " fullWidth>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={userInfo.activity}
-            onChange={(e, value) => {
-              handleChange("activity", value.props.value);
+            value={props.userInfo.activity}
+            onChange={(e) => {
+              props.handleChange("activity", e.target.value as string);
             }}
           >
-            {activityData.map((item) => (
+            {props.activityData.map((item) => (
               <MenuItem key={item} value={item}>
                 <em>{item}</em>
               </MenuItem>
--- src/app/components/WeightGroupButton/WeightGroupButton.tsx
+++ src/app/components/WeightGroupButton/WeightGroupButton.tsx
@@ -1,30 +1,31 @@
 import { FormControl, MenuItem, Select } from "@mui/material";
 import React from "react";
 import { useLocale } from "../providers/LanguageContext";
+import { UserInfo } from "../types";
 
-export default function WeightGroupButton({
-  handleChange,
-  userInfo,
-  weightData,
+export default function WeightGroupButton(props: {
+  handleChange: (key: string, value: number) => void;
+  userInfo: UserInfo;
+  weightData: number[];
 }) {
   const { chatWindow } = useLocale();
   return (
     <div>
       <h1 className="font-serif">Please specify your weight in KGs</h1>
       <div className=" w-1/2 ">
-        <FormControl color="neutral" className="bg-white " fullWidth>
+        <FormControl color="primary" className="bg-white " fullWidth>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={userInfo.weight}
-            onChange={(e, value) => {
-              handleChange("weight", value.props.value);
+            value={props.userInfo.weight}
+            onChange={(e) => {
+              props.handleChange("weight", e.target.value as number);
             }}
           >
             <MenuItem key={chatWindow.Weight} value={chatWindow.Weight}>
               <em>{chatWindow.Weight}</em>
             </MenuItem>
-            {weightData.map((item) => (
+            {props.weightData.map((item) => (
               <MenuItem key={item} value={item}>
                 {item}
               </MenuItem>
