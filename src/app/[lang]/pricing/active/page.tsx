d5b0736413aa603b0fa9d202cc0ef75b436af224
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-16 19:19:52 +0400

    feat(add): add subscription active ladning page

---
+++ src/app/[lang]/pricing/active/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ActivePage() {
+  return (
+    <div className="min-h-wrapper pt-52">
+      You have purchased our subscription
+    </div>
+  );
+}
