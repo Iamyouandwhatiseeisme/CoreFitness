7d6ec234f6951b416d38100214735294c2ba883b
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-16 19:22:15 +0400

    feat(add): add hosted checkout instead of embedded

--- src/app/components/CheckoutForm/CheckoutForm.tsx
+++ src/app/components/CheckoutForm/CheckoutForm.tsx
@@ -4,44 +4,22 @@ import type Stripe from "stripe";
 
 import React, { useState } from "react";
 
-import CustomDonationInput from "./CustomDonationInput";
-import StripeTestCards from "./StripeTestCards";
-
 import { formatAmountForDisplay } from "../../utils/stripe/stripe-helpers";
 import * as config from "../../../config";
 import { createCheckoutSession } from "../../actions/stripe";
-import getStripe from "../../utils/stripe/get-stripejs";
-import {
-  EmbeddedCheckout,
-  EmbeddedCheckoutProvider,
-} from "@stripe/react-stripe-js";
+import { Plan } from "../types";
 
 interface CheckoutFormProps {
   uiMode: Stripe.Checkout.SessionCreateParams.UiMode;
+  plan: Plan;
 }
 
 export default function CheckoutForm(props: CheckoutFormProps): JSX.Element {
   const [loading] = useState<boolean>(false);
-  const [input, setInput] = useState<{ customDonation: number }>({
-    customDonation: Math.round(config.MAX_AMOUNT / config.AMOUNT_STEP),
-  });
-  const [clientSecret, setClientSecret] = useState<string | null>(null);
-
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (
-    e
-  ): void =>
-    setInput({
-      ...input,
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
+  console.log(props.plan.priceId);
 
   const formAction = async (data: FormData): Promise<void> => {
-    const uiMode = data.get(
-      "uiMode"
-    ) as Stripe.Checkout.SessionCreateParams.UiMode;
-    const { client_secret, url } = await createCheckoutSession(data);
-
-    if (uiMode === "embedded") return setClientSecret(client_secret);
+    const { url } = await createCheckoutSession(data, props.plan);
 
     window.location.assign(url as string);
   };
@@ -50,33 +28,15 @@ export default function CheckoutForm(props: CheckoutFormProps): JSX.Element {
     <>
       <form action={formAction}>
         <input type="hidden" name="uiMode" value={props.uiMode} />
-        <CustomDonationInput
-          className="checkout-style"
-          name="customDonation"
-          min={config.MIN_AMOUNT}
-          max={config.MAX_AMOUNT}
-          step={config.AMOUNT_STEP}
-          currency={config.CURRENCY}
-          onChange={handleInputChange}
-          value={input.customDonation}
-        />
-        <StripeTestCards />
+
         <button
           className="checkout-style-background"
           type="submit"
           disabled={loading}
         >
-          Donate {formatAmountForDisplay(input.customDonation, config.CURRENCY)}
+          {props.plan.reccurence} Subscription{" "}
         </button>
       </form>
-      {clientSecret ? (
-        <EmbeddedCheckoutProvider
-          stripe={getStripe()}
-          options={{ clientSecret }}
-        >
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
-      ) : null}
     </>
   );
 }
