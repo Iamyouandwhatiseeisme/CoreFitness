d10c605f5698fe47788bc051bf0fbcd3477c4d9a
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-16 19:20:41 +0400

    style(add): customize style for pricing page add options for yearly and monthly payments

--- src/app/[lang]/pricing/page.tsx
+++ src/app/[lang]/pricing/page.tsx
@@ -1,9 +1,55 @@
 import CheckoutForm from "../../components/CheckoutForm/CheckoutForm";
+import { Plan } from "../../components/types";
+import { formatAmountForDisplay } from "../../utils/stripe/stripe-helpers";
+import * as config from "../../../config";
+import { createCheckoutSession } from "../../actions/stripe";
 
 export default function DonatePage(): JSX.Element {
+  const plans: Plan[] = [
+    {
+      reccurence: "Monthly",
+      price: 100,
+      priceId: process.env.PRICE_ID_MONTHLY!,
+      payementFrequency: "Every Month",
+    },
+    {
+      reccurence: "Yearly",
+      price: 800,
+      priceId: process.env.PRICE_ID_YEARLY!,
+      payementFrequency: "Every Year",
+    },
+  ];
+  // const handleCheckout = async (plan: Plan): Promise<void> => {
+  //   const data = new FormData();
+  //   data.append("uiMode", "hosted");
+  //   data.append("priceId", plan.priceId);
+  //   const { url } = await createCheckoutSession(data, plan);
+
+  //   window.location.assign(url as string);
+  // };
   return (
-    <div className="min-h-wrapper flex flex-col items-center justify-center">
-      <CheckoutForm uiMode="embedded" />
+    <div className="min-h-wrapper flex flex-row items-center justify-center">
+      {plans.map((plan) => {
+        console.log(plan.priceId);
+        return (
+          <div
+            key={plan.price}
+            // onClick={() => handleCheckout(plan)}
+            className=" p-10 mt-20 mr-10 ml-10 w-64 h-96 flex flex-col items-center justify-start rounded-2xl border bg-gray-300"
+          >
+            <div className="border-b w-48 h-24 items-center flex flex-row justify-center border-gray-800 border-opacity-25 rounded-2xl bg-gray-200 shadow-inner shadow-white">
+              <CheckoutForm uiMode="hosted" plan={plan}></CheckoutForm>
+            </div>
+            <div className="p-10 border-b border-gray-800 border-opacity-25">
+              Pay {formatAmountForDisplay(plan.price, config.CURRENCY)}{" "}
+              {plan.payementFrequency}
+            </div>
+            <div className=" border-b text-center border-gray-800 border-opacity-25">
+              Unlimited access to every branch, trainers, pool
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
