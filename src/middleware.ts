28b0d13b33bd57dd011d1ef58f4865a9811a72d1
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-21 18:13:31 +0400

    cleanup(logs): cleanup logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,7 +37,6 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
   }
   if (!user && pathname !== `/${locale}/login`) {
-    console.log("redirecting");
     const url = request.nextUrl.clone();
     url.pathname = `/${locale}/login`;
     return NextResponse.redirect(url);
