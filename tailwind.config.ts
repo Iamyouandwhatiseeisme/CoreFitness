eb211256162b86861a38502ce65385b7e126c17a
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-22 16:49:09 +0400

    bug(fix): fix tailwind, tsconfig and global.css bugs caused by shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  darkMode: "selector",
+  darkMode: ["selector"],
   theme: {
     extend: {
       keyframes: {
         scroll: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
         },
         slideIn: {
           "0%": {
@@ -29,7 +33,6 @@ module.exports = {
             borderBottomRightRadius: "50px",
           },
         },
-
         firstScrollHeaderReverse: {
           "0%": {
             borderBottomLeftRadius: "50px",
@@ -58,9 +61,48 @@ module.exports = {
       },
       colors: {
         "dark-header": "rgb(17, 14, 14)",
-        // "header-hover-dark": "rgba(255, 255, 255, 0.2)",
         "header-hover-dark": "#232323",
         "header-green": "#276749",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
       },
       transitionProperty: {
         "header-hover-transition": "background-color 0.5s",
@@ -72,14 +114,13 @@ module.exports = {
         "40vh": "40vh",
       },
       height: {
+        "120": "500px",
+        "150": "650px",
         "60vh": "60vh",
         "80vh": "80vh",
         "90vh": "90vh",
         "100vh": "100vh",
-        120: "500px",
-        150: "650px",
       },
-
       width: {
         "info-board-hover-width": "550px",
         "60wv": "60vw",
@@ -87,21 +128,24 @@ module.exports = {
         "150": "1200px",
       },
       scale: {
-        1.02: "1.02",
+        "1.02": "1.02",
       },
       borderRadius: {
-        50: "50px",
+        "50": "50px",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       boxShadow: {
         "small-black-shadow": "0 4px 6px rgba(0, 0, 0, 0.1);",
       },
       transitionDelay: {
-        0.3: "0.03s",
+        "0.3": "0.03s",
       },
       transitionDuration: {
-        1500: "1500ms",
+        "1500": "1500ms",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
