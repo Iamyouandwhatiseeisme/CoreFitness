1bc2be2c89acb0df7b50bf127de475da35c12429
iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-25 18:04:53 +0400

    feat(add): change order fetching to order fetching for the client using the app

--- src/app/[lang]/api/orders/route.ts
+++ src/app/[lang]/api/orders/route.ts
@@ -3,9 +3,15 @@ import { createClient } from "../../../utils/supabase/server";
 
 export async function GET() {
   const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   try {
-    const { data, error } = await supabase.from("orders").select();
+    const { data, error } = await supabase
+      .from("orders")
+      .select()
+      .eq("user_id", user?.id);
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json(
