6cb5dc4397de59e14a1f873a8534acc5a2dfca29
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-12-28 02:09:03 +0400

    feat(add): add OrderCards so orders are displayed correctly

--- src/app/[lang]/orders/page.tsx
+++ src/app/[lang]/orders/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Order } from "src/app/components/types";
+import { Order, OrderProducts, Product } from "src/app/components/types";
 
 export default function Orders() {
-  const [orders, setOrders] = useState<Order[][]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const router = useRouter();
+  // const router = useRouter();
   useEffect(() => {
     async function fetchOrders() {
       const response = await fetch("/api/orders", {
@@ -30,40 +30,92 @@ export default function Orders() {
       ) : (
         <div>
           {orders.map((order, index) => {
-            const formattedDate = new Date(
-              order[index].created_at
-            ).toLocaleDateString("en-CA");
+            return (
+              <OrderCard
+                key={order.id}
+                products={order.products}
+                order_id={order.id}
+              ></OrderCard>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+interface OrderCardProps {
+  products: OrderProducts[];
+  order_id: number;
+}
+
+export function OrderCard(props: OrderCardProps) {
+  const [products, setProducts] = useState<
+    Array<{ product: Product; quantity: number }>
+  >([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const productsToFetch = props.products;
+
+  useEffect(() => {
+    async function fetchOrders() {
+      let tempProducts: Array<{ product: Product; quantity: number }> = [];
+      for (const product of productsToFetch) {
+        const response = await fetch("/api/products", {
+          method: "GET",
+          headers: {
+            product_id: JSON.stringify(product.product_id),
+          },
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          if (responseData) {
+            const isProductInArray = tempProducts.some(
+              (item) => item.product.id === responseData.id
+            );
+
+            if (!isProductInArray) {
+              tempProducts.push({
+                product: responseData[0],
+                quantity: product.quantity,
+              });
+            }
+          }
+        }
+      }
+      setProducts(tempProducts);
+
+      setIsLoading(false);
+    }
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="min-h-wrapper bg-red-200 h-auto rounded-xl">
+      <div className="w-full border-b border-gray-200 p-5">
+        Order: # {props.order_id}
+      </div>
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : products.length === 0 ? (
+        <div>No products found</div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {products.map((productItem, index) => {
+            const { product, quantity } = productItem;
             return (
               <div
-                key={order[index].id}
-                className="h-120 w-150 rounded-2xl border border-gray-800 bg-white flex flex-col"
+                key={index}
+                className="border rounded-lg p-4 flex flex-col items-center"
               >
-                <div className="flex flex-row w-full items-center justify-between border-b-2 p-5">
-                  <div className="">
-                    Order Id: #{order[index].stripe_purchase_id}
-                  </div>
-                  <div
-                    className="underline flex cursor-pointer"
-                    onClick={() => {
-                      router.push(`/orders/${order[index].stripe_purchase_id}`);
-                    }}
-                  >
-                    See Details
-                  </div>
-                </div>
-                <div className="grid grid-cols-4 gap-4">
-                  {order.map((order) => {
-                    return (
-                      <div key={order.id} className="p-5">
-                        <div className="w-full h-40 rounded bg-gray-200 flex flex-col  items-center justify-center gap-2 border shadow-sm shadow-black">
-                          <div>Name : {order.title}</div>
-                          <div>Price: {order.price / 100}$</div>
-                          <div>Date: {formattedDate}</div>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
+                <img
+                  src={product.img_url}
+                  alt={product.title}
+                  className="w-40 h-40 object-cover mb-4"
+                />
+                <h3 className="text-xl font-semibold">{product.title}</h3>
+                <p className="text-lg text-gray-600">${product.price / 100}</p>
+                <p className="text-sm text-gray-500">Quantity: {quantity}</p>
               </div>
             );
           })}
@@ -73,38 +125,41 @@ export default function Orders() {
   );
 }
 
-// orders.map((orderGroup, index) => {
-//   const formattedDate = new Date(order.created_at).toLocaleDateString(
-//     "en-CA"
-//   );
+// {orders.map((order, index) => {
+//   const formattedDate = new Date(
+//     order[index].created_at
+//   ).toLocaleDateString("en-CA");
 //   return (
 //     <div
-//       key={order.created_at}
-//       className=" relative border-gray-400 h-28  rounded-2xl border  w-150 p-5 m-10 flex flex-col items-center"
+//       key={order[index].id}
+//       className="h-120 w-150 rounded-2xl border border-gray-800 bg-white flex flex-col"
 //     >
-//       <div className="w-full  border-b-2 border-gray-300 flex flex-row justify-between gap-2">
-//         <div className="flex flex-row gap-2">
-//           <div className="">{order.title}</div>
-//           <div>Price: {order.price / 100}$</div>
+//       <div className="flex flex-row w-full items-center justify-between border-b-2 p-5">
+//         <div className="">
+//           Order Id: #{order[index].stripe_purchase_id}
 //         </div>
-
 //         <div
-//           className="underline cursor-pointer"
+//           className="underline flex cursor-pointer"
 //           onClick={() => {
-//             router.push(`/orders/${order.id}`);
+//             router.push(`/orders/${order[index].stripe_purchase_id}`);
 //           }}
 //         >
-//           See details
+//           See Details
 //         </div>
 //       </div>
-
-//       <div className="absolute bottom-0 p-3 items-end justify-between flex flex-row  w-full">
-//         <div className="flex flex-row  p-2">Order ID: #{order.id}</div>
-//         <div className="flex flex-row  p-2">
-//           Order Time: {formattedDate}
-//         </div>
+//       <div className="grid grid-cols-4 gap-4">
+//         {order.map((order) => {
+//           return (
+//             <div key={order.id} className="p-5">
+//               <div className="w-full h-40 rounded bg-gray-200 flex flex-col  items-center justify-center gap-2 border shadow-sm shadow-black">
+//                 {/* <div>Name : {order.title}</div> */}
+//                 <div>Price: {order.total_price / 100}$</div>
+//                 <div>Date: {formattedDate}</div>
+//               </div>
+//             </div>
+//           );
+//         })}
 //       </div>
 //     </div>
 //   );
-// })
-// )}
+// })}
