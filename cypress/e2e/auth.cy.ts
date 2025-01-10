dc42f9bf03be8fc7fd6ce2da63ab5577cd2b4c26
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-10 12:59:23 +0400

    test(add): add test that tests user logout

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -7,4 +7,14 @@ describe("Auth", () => {
     cy.get("[data-cy='login-button']").click();
     cy.get("[data-cy='logout-button']").should("exist");
   });
+  it("Logs User Out successfully", () => {
+    cy.viewport(1536, 864);
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy='email']").type("karachka2@gmail.com");
+    cy.get("[data-cy='password']").type("Karachka2");
+    cy.get("[data-cy='login-button']").click();
+    cy.get("[data-cy='logout-button']").should("exist");
+    cy.get("[data-cy='logout-button']").click();
+    cy.get("[data-cy='login-button']").should("exist");
+  });
 });
