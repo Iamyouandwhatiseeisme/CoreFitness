8fe1043a53164cd67048a237b4f2077d0953f735
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-10 16:46:52 +0400

    feat(add): add handler for user deletion for profile page

--- src/app/[lang]/profile/page.tsx
+++ src/app/[lang]/profile/page.tsx
@@ -2,11 +2,12 @@
 import { useEffect, useState } from "react";
 import { createClient } from "../../utils/supabase/client";
 
-import "./index.css";
 import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
 
 export default function Profile() {
   const [user, setUser] = useState<User | null>();
+  const router = useRouter();
   const supabase = createClient();
   useEffect(() => {
     async function fetchUser() {
@@ -17,6 +18,12 @@ export default function Profile() {
     }
     fetchUser();
   }, []);
+  async function handleUserDeletion() {
+    const response = await fetch("/api/deleteUser");
+    if (response.ok) {
+      router.push("/login");
+    }
+  }
 
   return (
     <div className="min-h-wrapper">
@@ -24,6 +31,13 @@ export default function Profile() {
         {user ? (
           <>
             <div>{user.email}</div>
+            <div
+              onClick={() => handleUserDeletion()}
+              className=" cursor-pointer w-40 h-10 border rounded-2xl bg-black text-white flex flex-row items-center justify-center"
+              data-cy="delete-user-button"
+            >
+              Delete User
+            </div>
           </>
         ) : (
           <div>"loading"</div>
