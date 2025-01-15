0b0359f638ce7f823bad1445ef37ebd9c30936d2
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-15 18:24:41 +0400

    fix(module not found): fix module not found bug on searchBar

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import useDebounce from "../../hooks/useDebounce";
+import useDebounce from "src/app/hooks/useDebounce";
 
 interface SearchBarProps {
   searchItemType: string;
