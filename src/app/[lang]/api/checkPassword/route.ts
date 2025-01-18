c2d9bafe63d4f5fb50ad61fe8b3aa170d456ab41
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-18 18:32:49 +0400

    feat(add): add api endpoint for checking if the users password is correct in change password dialog

---
+++ src/app/[lang]/api/checkPassword/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "../../../utils/supabase/server";
+
+export async function GET(request: NextRequest) {
+  const currentPassword = request.headers.get("currentPassword");
+
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      if (user?.email !== undefined) {
+        if (currentPassword && currentPassword.length > 5) {
+          const { data: signInData, error: signInError } =
+            await supabase.auth.signInWithPassword({
+              email: user?.email,
+              password: currentPassword,
+            });
+
+          if (signInError) {
+            return NextResponse.json({
+              code: signInError.code,
+              status: signInError.status,
+            });
+          }
+          if (!signInError) {
+            console.log("no error");
+            return NextResponse.json({ code: signInData, status: 200 });
+          }
+        }
+        return NextResponse.json({
+          code: "Your password must be at least 6 characters long",
+          status: 400,
+        });
+      }
+    }
+
+    return NextResponse.json({
+      code: "Please provide a correct password password",
+      status: 400,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
