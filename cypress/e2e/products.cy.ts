706836f69695d7239b6df1bdb55f342cb5ae516b
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-24 21:06:24 +0400

    fix products test

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -27,9 +27,10 @@ describe("Product Actions", () => {
     cy.wait(2000);
     cy.get("[data-cy='TestProduct'").should("exist");
     cy.wait(2000);
+    cy.get("[data-cy='TestProduct']").should("exist");
 
     cy.get("[data-cy='TestProduct']").click();
-    cy.wait(1500);
+    cy.wait(4000);
     cy.get("[data-cy='delete-button'").click();
     cy.get("[data-cy='add-product-button'").should("exist");
   });
