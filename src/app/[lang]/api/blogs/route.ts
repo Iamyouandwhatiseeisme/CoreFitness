eade315d6b141202487722113a3d8e78ba60230c
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-28 18:13:29 +0400

    feat(add): add api endpoint for blogs fetching

---
+++ src/app/[lang]/api/blogs/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { createClient } from "../../../utils/supabase/server";
+
+export async function GET() {
+  const supabase = await createClient();
+
+  try {
+    const { data } = await supabase.from("blogs").select();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
