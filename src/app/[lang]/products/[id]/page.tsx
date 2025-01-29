16d58d333184f91b7c30c43355fc4f963bace322
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-29 18:39:58 +0400

    refr(change): adjust products details page to more abstract components for editing and deleting products

--- src/app/[lang]/products/[id]/page.tsx
+++ src/app/[lang]/products/[id]/page.tsx
@@ -3,7 +3,7 @@ import NotFound from "../../notfound/NotFound";
 import { ReturnBackButton } from "../../notfound/NotFound";
 import fetchSingleProduct from "../../../fetcher/fetchSingleProduct";
 import { Product } from "../../../components/types";
-import { DeleteProductButton } from "src/app/components/DeleteProductButton/DeleteProductButton";
+import { DeleteItem } from "src/app/components/DeleteItem/DeleteItem";
 import { createClient } from "src/app/utils/supabase/server";
 import EditProductDIalog from "src/app/components/EditProductDialog/EditProductDialog";
 import { Toaster } from "sonner";
@@ -22,15 +22,12 @@ export default async function ProductPage(props: ProductDetailsPageProps) {
   } = await supabase.auth.getUser();
 
   const product = (await fetchSingleProduct(id)) as Product | null;
-  if (user) {
-    console.log(user.id, product?.user_id, "id");
-  }
 
   if (!product) return <NotFound page="products" />;
   return (
     <div className="gap-4 min-h-wrapper pt-40 flex flex-col items-center justify-center ">
       <h1 className="underline cursor-pointer font-serif font-bold text-2xl">
-        {product.title}
+        {props.params.lang === "ka" ? product.title_ka : product.title}
       </h1>
       <img
         className="w-80 h-80 "
@@ -49,7 +46,7 @@ export default async function ProductPage(props: ProductDetailsPageProps) {
         {user && user.id === product.user_id ? (
           <div className="flex flex-row gap-2">
             {" "}
-            <DeleteProductButton id={id}></DeleteProductButton>
+            <DeleteItem id={id} table="products"></DeleteItem>
             <EditProductDIalog product={product}></EditProductDIalog>
           </div>
         ) : (
