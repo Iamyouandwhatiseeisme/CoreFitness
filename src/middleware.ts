59fa309475f6cb85d2085eab48f57d05209df922
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-29 17:37:42 +0400

    cleanup(debuggin): cleanup debugging console logs and fix middleware order

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,10 +11,8 @@ const locales = ["en-US", "ka"];
 function getLocale(request: NextRequest): string {
   const requestHeaders = new Headers(request.headers);
   let accepted = requestHeaders.get("accept-language") || "";
-  console.log(request.nextUrl.pathname, "accepted");
 
   if (request.nextUrl.pathname.startsWith("ka")) {
-    console.log(request.nextUrl.pathname, "accepted");
     accepted = "ka";
   }
 
@@ -27,102 +25,90 @@ function getLocale(request: NextRequest): string {
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  // const locale = getLocale(request);
-  // console.log(locale, "locale");
 
-  const sessionResponse = await updateSession(request);
-  const userHeader = sessionResponse.headers.get("user");
-  const user = userHeader ? JSON.parse(userHeader) : null;
-  // const publicPaths = [
-  //   `/${locale}/login`,
-  //   `/${locale}/login/passwordRecovery`,
-  //   `/${locale}/api/passwordRecovery`,
-  //   `/login`,
-  //   `/login/passwordRecovery`,
-  //   `/api/passwordRecovery`,
-  // ];
-
-  // if (
-  //   pathname.startsWith(`/auth/callback`) ||
-  //   pathname.startsWith("/auth/auth-code-error")
-  // ) {
-  //   return NextResponse.next();
-  // }
-  // if (!user && !publicPaths.includes(pathname)) {
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = `/${locale}/login`;
-  //   return NextResponse.redirect(url);
-  // }
-  // if (user && pathname === `/${locale}/login`) {
-  //   request.nextUrl.pathname = `/${locale}`;
-  //   return NextResponse.redirect(request.nextUrl);
-  // }
-
-  // if (
-  //   user &&
-  //   (pathname === `/${locale}/pricing` ||
-  //
-  // pathname === `/${locale}/pricing/active`)
-  // ) {
-  //   const status: SubscriptionStatus = await isStripeSubscriptionActive(
-  //     user?.email!
-  //   );
-  //   if (
-  //     status === SubscriptionStatus.Active &&
-  //     pathname === `/${locale}/pricing`
-  //   ) {
-  //     request.nextUrl.pathname = `/${locale}/pricing/active`;
-  //     return NextResponse.redirect(request.nextUrl);
-  //   } else if (
-  //     status === SubscriptionStatus.Inactive &&
-  //     pathname === `/${locale}/pricing/active`
-  //   ) {
-  //     request.nextUrl.pathname = `/${locale}/pricing`;
-  //     return NextResponse.redirect(request.nextUrl);
-  //   }
-  // }
   if (request.nextUrl.pathname.startsWith("/api")) {
     return NextResponse.next();
   }
   const pathnameSegments = pathname.split("/");
   const pathnameLocale = pathnameSegments[1];
   const locale = locales.includes(pathnameLocale) ? pathnameLocale : "en-US";
-  console.log(locale, "locale in path");
 
+  const sessionResponse = await updateSession(request);
+  const userHeader = sessionResponse.headers.get("user");
+  const user = userHeader ? JSON.parse(userHeader) : null;
+  const publicPaths = [
+    `/${locale}/login`,
+    `/${locale}/login/passwordRecovery`,
+    `/${locale}/api/passwordRecovery`,
+    `/login`,
+    `/login/passwordRecovery`,
+    `/api/passwordRecovery`,
+  ];
+
+  if (
+    pathname.startsWith(`/auth/callback`) ||
+    pathname.startsWith("/auth/auth-code-error")
+  ) {
+    return NextResponse.next();
+  }
+  if (!user && !publicPaths.includes(pathname)) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${locale}/login`;
+    return NextResponse.redirect(url);
+  }
+  if (user && pathname === `/${locale}/login`) {
+    console.log("user trying to access login page");
+    request.nextUrl.pathname = `/${locale}`;
+    return NextResponse.redirect(request.nextUrl);
+  }
+
+  if (
+    user &&
+    (pathname === `/${locale}/pricing` ||
+      pathname === `/${locale}/pricing/active`)
+  ) {
+    const status: SubscriptionStatus = await isStripeSubscriptionActive(
+      user?.email!
+    );
+    if (
+      status === SubscriptionStatus.Active &&
+      pathname === `/${locale}/pricing`
+    ) {
+      request.nextUrl.pathname = `/${locale}/pricing/active`;
+      return NextResponse.redirect(request.nextUrl);
+    } else if (
+      status === SubscriptionStatus.Inactive &&
+      pathname === `/${locale}/pricing/active`
+    ) {
+      request.nextUrl.pathname = `/${locale}/pricing`;
+      return NextResponse.redirect(request.nextUrl);
+    }
+  }
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
-  console.log(request.nextUrl.pathname, pathnameHasLocale, locale);
 
-  console.log(1, request.nextUrl.pathname);
   if (pathnameHasLocale) {
     if (pathnameSegments[2]) {
       if (pathnameSegments[2] === pathnameLocale) {
         const response = NextResponse.next();
         response.headers.set("accept-Language", locale);
-        console.log(response.headers, "headers");
 
         request.nextUrl.pathname = `/${locale}/${pathnameSegments[3]}/${pathnameSegments[4]}`;
-
-        console.log(request.nextUrl.pathname, "pathname");
 
         return NextResponse.redirect(request.nextUrl);
       }
     }
-  }
-
-  if (pathnameHasLocale) {
     const response = NextResponse.next();
     response.headers.set("accept-Language", locale);
-    console.log(response.headers, "headers");
     return response;
   }
+  if (!pathnameHasLocale) {
+    const loc = getLocale(request);
+    request.nextUrl.pathname = `/${loc}${pathname}`;
 
-  const loc = getLocale(request);
-  console.log(locale);
-  request.nextUrl.pathname = `/${loc}${pathname}`;
-
-  return NextResponse.redirect(request.nextUrl);
+    return NextResponse.redirect(request.nextUrl);
+  }
 }
 
 export const config = {
