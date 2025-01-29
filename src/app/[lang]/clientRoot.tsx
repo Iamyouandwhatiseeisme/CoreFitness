22e4115f9d16fa04f12e134501ed11f8c937d983
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-29 21:21:10 +0400

    fix(deployment): fix deployment erorr

--- src/app/[lang]/clientRoot.tsx
+++ src/app/[lang]/clientRoot.tsx
@@ -6,8 +6,22 @@ import { CartProvider } from "../components/providers/CartProvider";
 import { LocaleProvider } from "../components/providers/LanguageContext";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 import { Dictionary } from "./dictionaries";
 import { User } from "@supabase/supabase-js";
+
+function ThemeWrapper({ children }: ThemeProviderProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
 
 export default function ClientRoot({
   children,
@@ -29,32 +43,25 @@ export default function ClientRoot({
   if (!mounted)
     return (
       <div className="flex justify-center items-center min-h-screen bg-neutral-200 dark:bg-neutral-900">
-        <div className="animate-spin rounded-full h-12 w-12 border-4 border-solid border-current border-r-transparent dark:border-r-transparent"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-solid border-current border-r-transparent"></div>
       </div>
     );
 
   return (
-    <div>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
+    <ThemeWrapper>
+      <LocaleProvider
+        lang={lang}
+        dictChat={dict.chatWindow}
+        informationBoard={dict.informationBoard}
       >
-        <LocaleProvider
-          lang={lang}
-          dictChat={dict.chatWindow}
-          informationBoard={dict.informationBoard}
-        >
-          <CartProvider>
-            <div className="bg-neutral-200 dark:bg-neutral-900 font-serif">
-              <Header currentUser={user} dict={dict.header} />
-              {children}
-              <Footer />
-            </div>
-          </CartProvider>
-        </LocaleProvider>
-      </ThemeProvider>
-    </div>
+        <CartProvider>
+          <div className="bg-neutral-200 dark:bg-neutral-900 font-serif">
+            <Header currentUser={user} dict={dict.header} />
+            {children}
+            <Footer />
+          </div>
+        </CartProvider>
+      </LocaleProvider>
+    </ThemeWrapper>
   );
 }
