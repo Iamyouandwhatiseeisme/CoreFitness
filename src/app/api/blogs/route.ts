eef8fcace832ef9e27f9e031759a62a395d08625
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-01-31 01:11:55 +0400

    style(apply): apply somee styles to blogspage, add email to card

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -5,7 +5,11 @@ export async function GET() {
   const supabase = await createClient();
 
   try {
-    const { data } = await supabase.from("blogs").select();
+    const { data } = await supabase
+      .from("blogs")
+      .select()
+      .order("created_at", { ascending: false });
+    console.log(data);
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
