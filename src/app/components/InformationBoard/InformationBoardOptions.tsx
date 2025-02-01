1fdcc9da2c995046b328219f0871be4f0848d0b8
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-01 17:34:52 +0400

    style(add): add hover animation to informationBoard

--- src/app/components/InformationBoard/InformationBoardOptions.tsx
+++ src/app/components/InformationBoard/InformationBoardOptions.tsx
@@ -18,18 +18,18 @@ export default function InformationBoardOptions(
       style={{
         backgroundColor: props.isHovered ? props.hoverColor : "transparent",
         transition: "transform 0.15s ease-in-out",
-        width: props.isHovered ? "480px" : "",
+        width: props.isHovered ? "calc(100% + 20px)" : "",
         transform: props.isHovered ? "scale(1.05)" : "scale(1.0)",
         height: props.isHovered ? "384px" : "",
         transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
       }}
-      className={` rounded-3xl  hover:z-10 cursor-pointer`}
+      className={`rounded-3xl hover:z-10 cursor-pointer`}
     >
-      <div className="flex justify-start gap-10 m-8 flex-row h-14  ">
+      <div className="flex justify-start gap-10 m-8 flex-row h-14">
         <div className="w-16 flex flex-col justify-center">
           <img
             src={props.item.logo}
-            className="rounded-full w-14  "
+            className="rounded-full w-14"
             alt="fitpass"
           ></img>
         </div>
