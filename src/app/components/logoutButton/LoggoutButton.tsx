6c55150ce0a69359f11e0cdd44e9f7fb579b1b28
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-01 18:11:44 +0400

    refr(delete): delete type check for logout button

--- src/app/components/logoutButton/LoggoutButton.tsx
+++ src/app/components/logoutButton/LoggoutButton.tsx
@@ -18,9 +18,7 @@ export default function AuthenticationButton(props: AuthenticationButtonProps) {
     <a href={props.href}>
       <button
         onClick={() => {
-          if (props.type === "logout") {
-            signOut();
-          }
+          signOut();
         }}
         className="w-14 h-14 items-center justify-center flex mt-2 text-black dark:text-white bg-white/20 border-[#5A5B5C]   shadow-sm shadow-blue-400/20 dark:shadow-yellow-400/20 dark:hover:bg-[#4E4F50]  hover:bg-white/40 hover:scale-105 transition-all duration-300  rounded-md cursor-pointer"
       >
