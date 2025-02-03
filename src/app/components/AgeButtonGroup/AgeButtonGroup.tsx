2acc6af51c8711f5b52feaa3cf1b218e6832bf9b
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-03 19:38:17 +0400

    style(add): improve style for chatWindow

--- src/app/components/AgeButtonGroup/AgeButtonGroup.tsx
+++ src/app/components/AgeButtonGroup/AgeButtonGroup.tsx
@@ -13,28 +13,36 @@ export default function AgeButtonGroup(props: AgeButtonGroupProps) {
   const { chatWindow } = useLocale();
 
   return (
-    <div>
-      <FormControl color="primary" className="bg-white " fullWidth>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={props.userInfo.age}
-          onChange={(e) => {
-            props.handleChange("age", e.target.value as number);
-          }}
-        >
-          <MenuItem key="Age" value={chatWindow.Age}>
-            <em>{chatWindow.Age}</em>
-          </MenuItem>
-          {props.ageData.map((item) => {
-            return (
-              <MenuItem key={item} value={item}>
-                <em>{item}</em>
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+    <div className="flex flex-col w-3/5 h-3/5 rounded-2xl m-5 items-center border justify-center bg-gray-400 bg-opacity-20">
+      <label className="text-lg font-bold text-center text-gray-800 dark:text-gray-100  bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+        Please specify your:{" "}
+      </label>
+      <div className="shadow-lg shadow-slate-800 rounded-2xl m-2">
+        <FormControl color="primary" className="bg-white " fullWidth>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={props.userInfo.age}
+            onChange={(e) => {
+              props.handleChange("age", e.target.value as number);
+            }}
+          >
+            <MenuItem key="Age" value={chatWindow.Age}>
+              <em>{chatWindow.Age}</em>
+            </MenuItem>
+            {props.ageData.map((item) => {
+              return (
+                <MenuItem key={item} value={item}>
+                  <em>{item}</em>
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </div>
+      <label className="text-lg font-bold text-center text-gray-800 dark:text-gray-100  bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+        Age{" "}
+      </label>
     </div>
   );
 }
--- src/app/components/MuscleGoalButtonGroup/MuscleGoalButtonGroup.tsx
+++ src/app/components/MuscleGoalButtonGroup/MuscleGoalButtonGroup.tsx
@@ -1,4 +1,4 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { FormControl, MenuItem, Select } from "@mui/material";
 
 import React from "react";
 import { UserInfo } from "../types";
@@ -11,49 +11,27 @@ export default function MuscleGoalButtonGroup(
   props: MuscleGoalButtonGroupProps
 ) {
   return (
-    <div className="flex flex-col w-1/2 h-3/5 rounded-2xl m-5 items-center border justify-center bg-gray-400 bg-opacity-20 ">
+    <div className="flex flex-col w-3/5 h-3/5 rounded-2xl m-5 items-center border justify-center bg-gray-400 bg-opacity-20 ">
       <label className="text-lg font-bold text-center text-gray-800 dark:text-gray-100  bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
         Your primary goal is to:{" "}
       </label>
       <div className="shadow-lg shadow-slate-800 rounded-2xl m-2">
-        <ToggleButtonGroup
-          className="bg-blue-400"
-          size="large"
-          value={props.userInfo.muscleGoal}
-          exclusive
-          onChange={(e, value) => {
-            if (value !== null) {
-              props.handleChange("muscleGoal", value);
-            }
-          }}
-          aria-label="Platform"
-        >
-          {props.muscleGoalData.map((goal) => {
-            return (
-              <ToggleButton
-                key={goal}
-                className="text-black dark:text-black hover:transform hover:scale-105 transition-transform "
-                onMouseEnter={(e) => {
-                  if (props.userInfo.muscleGoal !== goal) {
-                    (e.target as HTMLElement).style.background = "#a3c9f9";
-                  }
-                }}
-                onMouseLeave={(e) => {
-                  if (props.userInfo.muscleGoal !== goal) {
-                    (e.target as HTMLElement).style.background = "#60A5FA";
-                  }
-                }}
-                value={goal}
-                style={{
-                  background:
-                    props.userInfo.muscleGoal === goal ? "White" : "#60A5FA",
-                }}
-              >
-                {goal}
-              </ToggleButton>
-            );
-          })}
-        </ToggleButtonGroup>
+        <FormControl color="primary" className="bg-white" fullWidth>
+          <Select
+            labelId="muscle-goal-select-label"
+            id="muscle-goal-select"
+            value={props.userInfo.muscleGoal}
+            onChange={(e) => {
+              props.handleChange("muscleGoal", e.target.value as string);
+            }}
+          >
+            {props.muscleGoalData.map((goal) => (
+              <MenuItem key={goal} value={goal}>
+                <em>{goal}</em>
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </div>
       <label className="text-lg font-bold text-center text-gray-800 dark:text-gray-100  bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
         Weight{" "}
