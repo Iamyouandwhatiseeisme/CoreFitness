b1839c0f01ad759e631fbf889f858468ec071691
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-06 20:35:55 +0400

    refr(update): update api endpoints to match new database objects

--- src/app/api/equipment/route.ts
+++ src/app/api/equipment/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   const supabase = await createClient();
 
   try {
-    const { data } = await supabase.from("gymEquipment").select();
+    const { data, error } = await supabase.from("gymEquipment").select();
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
---
+++ src/app/api/orders/singleOrder/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "src/app/utils/supabase/server";
+
+export async function GET(request: NextRequest) {
+  const id = request.headers.get("id");
+  try {
+    const supabase = await createClient();
+
+    const { data } = await supabase
+      .from("orders")
+      .select()
+      .eq("id", id)
+      .single();
+    console.log(data);
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
