ae53bde74166bec9429a0f3028f4d64ac20e1375
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-18 18:12:54 +0400

    style(add): add shadows to contact page

--- src/app/[lang]/contact/page.tsx
+++ src/app/[lang]/contact/page.tsx
@@ -32,7 +32,7 @@ export default function Contact() {
   return (
     <div className="min-h-wrapper flex sm:flex-row flex-col items-start pt-20 gap-5 sm:justify-evenly sm:pt-40">
       <Toaster></Toaster>
-      <div className="flex flex-col w-96 sm:pl-40">
+      <div className="flex flex-col w-96 sm:pl-40 shadow-sm shadow-black/20 dark:shadow-white rounded">
         <div>{contact.Phone}: +995 555 55 55 55</div>
         <div>{contact.Email} : saabashidze@gmail.com</div>
         <div>{contact.Address}</div>
@@ -47,7 +47,7 @@ export default function Contact() {
               onChange={(e) => setEmail(e.target.value)}
               name="from"
               type="email"
-              className="w-96 p-2 border border-gray-300 rounded"
+              className="w-96 p-2 border shadow-sm shadow-black/20 dark:shadow-white  border-gray-300 rounded"
               required
             ></input>
             <label>{contact.Subject}: </label>
@@ -57,22 +57,22 @@ export default function Contact() {
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
               minLength={5}
-              className="w-96 p-2 border border-gray-300 rounded"
+              className="w-96 p-2 border shadow-sm shadow-black/20 dark:shadow-white  border-gray-300 rounded"
               required
             ></input>
             <label>{contact.Content}: </label>
-            <div className="flex flex-row items-end gap-4">
+            <div className="flex sm:flex-row flex-col items-end gap-4">
               {" "}
               <textarea
                 name="content"
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
-                className="p-2 border border-gray-300 rounded resize-none h-40 w-3/5"
+                className="p-2 border border-gray-300 rounded shadow-sm shadow-black/20 dark:shadow-white  resize-none h-40 sm:w-3/5 w-full"
                 required
               ></textarea>{" "}
               <button
                 type="submit"
-                className="rouned border-black border text-black bg-slate-200 w-40 hover:bg-white"
+                className="rounded shadow-sm shadow-black/20 dark:shadow-white  border-black border text-black bg-slate-200 w-40 hover:bg-white"
               >
                 {contact.Send}
               </button>
