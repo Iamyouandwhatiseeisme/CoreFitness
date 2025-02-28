36fdbeb02e35a8ef613c0b792a35d3d7f416b9bc
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-28 12:54:51 +0400

    feat(add): add animation for equipment page description and title

--- src/app/[lang]/equipment/page.tsx
+++ src/app/[lang]/equipment/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useLocale } from "src/app/components/providers/LanguageContext";
+import { motion } from "framer-motion";
 
 interface Equipment {
   id: number;
@@ -17,6 +18,7 @@ export default function EquipmentPage() {
   const [equipment, setEquipment] = useState<Equipment[]>([]);
   const { locale } = useLocale();
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
     fetch("/api/equipment")
       .then((res) => res.json())
@@ -27,44 +29,88 @@ export default function EquipmentPage() {
   }, []);
 
   return (
-    <div className="w-full min-h-screen ">
+    <div className="w-full min-h-screen  ">
       {isLoading ? (
         <div className="flex justify-center items-center">
           <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-white"></div>
         </div>
       ) : (
-        <div className="flex flex-col  w-full ">
+        <div className="flex flex-col w-full ">
           {equipment.map((item) => {
             const description =
               locale === "ka" ? item.description_ka : item.description;
             return (
               <div
                 key={item.id}
-                className="w-full h-auto bg-white/20 dark:bg-gray-900/30 flex flex-col md:flex-row items-start justify-between border border-gray-500 dark:border-gray-600 rounded-2xl transition-colors duration-300"
+                className="w-full h-auto lex flex-col md:flex-row items-start justify-between border "
               >
                 <div className=" relative bg-gradient-to-r from-slate-50/50 to-gray-300/50 dark:from-gray-900/40 dark:via-gray-800/80 dark:to-gray-700/70 z-20  min-h-screen">
                   <img
-                    className="w-full  border-gray-200 dark:border-gray-700 object-cover z-10 "
+                    className="w-full bg-fixed border-gray-200 dark:border-gray-700 object-cover z-10 "
                     src={item.img.img}
                     key={item.id}
                     alt={`${item.id}1`}
                   />
-                  <div className="absolute inset-0 bg-black/60 z-20"></div>
-                </div>
+                  <Description locale={locale} item={item}></Description>
 
-                {/* //   <div className="w-full flex flex-col items-center mt-4 md:mt-10 border-gray-200 dark:border-gray-700">
-              //     <h1 className="font-bold text-2xl underline border-b-2 border-gray-200 dark:border-gray-700 text-gray-900 dark:text-gray-100">
-              //       {item.title}
-              //     </h1>
-              //     <p className="m-4 md:m-10 text-lg text-gray-800 dark:text-gray-300">
-              //       {description}
-              //     </p>
-              //   </div> */}
+                  <div className="absolute inset-0 bg-black/80 z-20"></div>
+                </div>
               </div>
             );
           })}
         </div>
       )}
+    </div>
+  );
+}
+
+interface DescriptionProps {
+  item: Equipment;
+  locale: string;
+}
+
+export function Description(props: DescriptionProps) {
+  const item = props.item;
+  const locale = props.locale;
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.getElementById(`description-${item.id}`);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+        console.log("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div>
+      <motion.div
+        className="absolute right-0 text-xl font-thin top-72 z-40 w-1/2 h-1/2"
+        id={`description-${item.id}`}
+        initial={{ opacity: 0, y: 80 }}
+        animate={isVisible ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+      >
+        {locale === "ka" ? item.description_ka : item.description}
+      </motion.div>
+      <motion.div
+        className="absolute right-0 text-xl font-thin top-40 z-40 w-1/2 h-1/2"
+        id={`description-${item.id}`}
+        initial={{ opacity: 0, x: 40 }}
+        animate={isVisible ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+      >
+        {locale === "ka" ? item.title_ka : item.title}
+      </motion.div>
     </div>
   );
 }
