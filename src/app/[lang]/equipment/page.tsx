598b07307904c709048c5fc0bdb059c7c795981c
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-02-28 13:12:51 +0400

    fix(bug): fix build bug where react component wasn't able to be compiled onbuild

--- src/app/[lang]/equipment/page.tsx
+++ src/app/[lang]/equipment/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Description from "src/app/components/Description/Description";
 import { useLocale } from "src/app/components/providers/LanguageContext";
-import { motion } from "framer-motion";
 
-interface Equipment {
+export interface Equipment {
   id: number;
   img: {
     img: string;
@@ -58,55 +58,6 @@ export default function EquipmentPage() {
           })}
         </div>
       )}
-    </div>
-  );
-}
-
-interface DescriptionProps {
-  item: Equipment;
-  locale: string;
-}
-
-export function Description(props: DescriptionProps) {
-  const item = props.item;
-  const locale = props.locale;
-  const [isVisible, setIsVisible] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      const element = document.getElementById(`description-${item.id}`);
-      if (element) {
-        const rect = element.getBoundingClientRect();
-        if (rect.top < window.innerHeight && rect.bottom > 0) {
-          setIsVisible(true);
-        } else {
-          setIsVisible(false);
-        }
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [item.id]);
-
-  return (
-    <div>
-      <motion.div
-        className="absolute right-0 text-xl font-thin top-72 z-40 w-1/2 h-1/2"
-        id={`description-${item.id}`}
-        initial={{ opacity: 0, y: 80 }}
-        animate={isVisible ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 1.5, ease: "easeInOut" }}
-      >
-        {locale === "ka" ? item.description_ka : item.description}
-      </motion.div>
-      <motion.div
-        className="absolute right-0 text-xl font-thin top-40 z-40 w-1/2 h-1/2"
-        id={`description-${item.id}`}
-        initial={{ opacity: 0, x: 40 }}
-        animate={isVisible ? { opacity: 1, x: 0 } : {}}
-        transition={{ duration: 1.5, ease: "easeInOut" }}
-      >
-        {locale === "ka" ? item.title_ka : item.title}
-      </motion.div>
     </div>
   );
 }
---
+++ src/app/components/Description/Description.tsx
@@ -0,0 +1,52 @@
+import { Equipment } from "src/app/[lang]/equipment/page";
+import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+
+interface DescriptionProps {
+  item: Equipment;
+  locale: string;
+}
+
+export default function Description(props: DescriptionProps) {
+  const item = props.item;
+  const locale = props.locale;
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.getElementById(`description-${item.id}`);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [item.id]);
+
+  return (
+    <div>
+      <motion.div
+        className="absolute right-0 text-xl font-thin top-72 z-40 w-1/2 h-1/2"
+        id={`description-${item.id}`}
+        initial={{ opacity: 0, y: 80 }}
+        animate={isVisible ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+      >
+        {locale === "ka" ? item.description_ka : item.description}
+      </motion.div>
+      <motion.div
+        className="absolute right-0 text-xl font-thin top-40 z-40 w-1/2 h-1/2"
+        id={`description-${item.id}`}
+        initial={{ opacity: 0, x: 40 }}
+        animate={isVisible ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+      >
+        {locale === "ka" ? item.title_ka : item.title}
+      </motion.div>
+    </div>
+  );
+}
