8e94d8c8b6f0e8a57a3e8c3e8e95ddfefabf8fb7
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-03-02 15:14:56 +0400

    fix(bug): fix bug where toaster wouldn't show up on products details page

--- src/app/components/AddToCart/AddToCart.tsx
+++ src/app/components/AddToCart/AddToCart.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { toast } from "sonner";
 import { useCart } from "../providers/CartProvider";
 import { useLocale } from "../providers/LanguageContext";
 import { Product } from "../types";
@@ -12,14 +13,19 @@ interface AddToCartProps {
 export default function AddToCart(props: AddToCartProps) {
   const { addItemToCart } = useCart();
   const {
-    dictionary: { products },
+    dictionary: { products, cart },
   } = useLocale();
 
   return (
     <button
       className="w-20 max-h-20 z-50 py-2 rounded-lg   dark:bg-white text-black border-black/20 border dark:hover:bg-blue-700 hover:text-white bg-blue-500/20 hover:bg-blue-600 transition-colors duration-300  hover:opacity-100"
       data-cy={`add-to-cart-button-${props.product.title}`}
-      onClick={() => addItemToCart({ product: props.product, quantity: 1 })}
+      onClick={() => {
+        addItemToCart({ product: props.product, quantity: 1 });
+        toast.success(cart.Added, {
+          description: `${props.product.title}\n ${props.product.title_ka}`,
+        });
+      }}
     >
       {products.AddToCart}
     </button>
