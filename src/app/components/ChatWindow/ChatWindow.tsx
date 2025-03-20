a16e7465ba989e39bae6604e96d07f82a22c4a99
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-03-20 16:25:17 +0400

    feat(update): update api version for gemini

--- src/app/components/ChatWindow/ChatWindow.tsx
+++ src/app/components/ChatWindow/ChatWindow.tsx
@@ -30,7 +30,7 @@ export default function ChatWindow() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_API_KEY!);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
   useEffect(() => {
     setUserInfo(userInfo);
